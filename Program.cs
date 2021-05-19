23c12950afe536fd9a1203b7554e371653adcd60
kevin <kevin1737@163.com>
2021-05-19 16:17:22 +0800

    Add: C# Notes

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
